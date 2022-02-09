7859ed13df499b8515f905bc2fc8865a8220b3f9
Dinusha <99312176+it21213212@users.noreply.github.com>
2022-02-09 06:08:37 +0000

    completedtute1

--- Tute01.c
+++ Tute01.c
@@ -4,8 +4,22 @@
 
 #include <stdio.h>
 
-int main() {
-  
+int main() 
+{
+  float mark1 , mark2 , sum = 0, average = 0;
+
+  printf("Enter the mark 1 :"); // get the input
+  scanf("%f" , &mark1);
+
+  printf("Enter the mark 2 :");  // get the input
+  scanf("%f" , &mark2);
+
+  sum = mark1 + mark2;  // calculation
+
+  average = sum/2;  // calculation
+
+  printf("Average is : %.2f\n" , average);  // print the average
+
   return 0;
 }
 
